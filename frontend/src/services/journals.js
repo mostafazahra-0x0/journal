13cc5fdc2a85e7108a1a6e9dc4df4f7f49251bf1
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-08-03 14:17:36 +0300

    feat(frontend): add create, update, and remove methods to journal service

--- frontend/src/services/journals.js
+++ frontend/src/services/journals.js
@@ -19,4 +19,28 @@ const getAll = () => {
   return request.then(response => response.data)
 }
 
-export default { setToken, getToken, getAll }
+const create = (content) => {
+  const config = {
+    headers: { Authorization: getToken() }
+  }
+  const request = axios.post(baseUrl, { content }, config)
+  return request.then(response => response.data)
+}
+
+const update = (id, content) => {
+  const config = {
+    headers: { Authorization: getToken() }
+  }
+  const request = axios.put(`${baseUrl}/${id}`, { content }, config)
+  return request.then(response => response.data)
+}
+
+const remove = (id) => {
+  const config = {
+    headers: { Authorization: getToken() }
+  }
+  const request = axios.delete(`${baseUrl}/${id}`, config)
+  return request.then(response => response.data)
+}
+
+export default { setToken, getToken, getAll, create, update, remove }

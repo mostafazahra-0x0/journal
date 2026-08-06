5d35ccf0d797387fe59d2c16fe3610f12c44a432
mostafazahra0x0 <mostafazahra.root.0x0@gmail.com>
2026-08-07 00:29:04 +0300

    style(frontend): add basic styling for journal list, form, and buttons

--- frontend/src/components/JournalForm.jsx
+++ frontend/src/components/JournalForm.jsx
@@ -1,16 +1,3 @@
-
-import styled from 'styled-components'
-
-const JournalTextArea = styled.textarea`
-  width: 100%;
-  height: 100%;
-  max-width: 700px;
-  height: 70vh;
-  border:5px solid ;
-  border-radius: 20px;
-  border-color: #463221;
-  background: #74634d;
-`
 const JournalForm = ({ 
   handleSubmit,
   content,
@@ -18,7 +5,7 @@ const JournalForm = ({
 }) => {
   return (
     <form onSubmit={handleSubmit}>
-      <JournalTextArea value={content} onChange={handleContentChange} />
+      <textarea value={content} onChange={handleContentChange} />
       <button type="submit">Save</button>
     </form>
   )
